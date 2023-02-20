1cd6855309ef1df55ed840dfc0d5947d4a30e7a4
Chris Smit <chris.smit@dimagi.com>
2023-02-20 13:31:55 +0200

    Add UI to query mobile workers as possible attendees

--- corehq/apps/events/urls.py
+++ corehq/apps/events/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import re_path as url
-from corehq.apps.events.views import EventsView, EventCreateView
+from corehq.apps.events.views import EventsView, EventCreateView, AttendeesAddView, paginate_possible_attendees
 
 urlpatterns = [
     url(r'^$', EventsView.as_view(), name=EventsView.urlname),
     url(r'^new/$', EventCreateView.as_view(), name=EventCreateView.urlname),
+    url(r'^attendees/$', AttendeesAddView.as_view(), name=AttendeesAddView.urlname),
+    url(r'^users/$', paginate_possible_attendees, name='paginate_possible_attendees'),
+
 ]
